bea9e15cba3d94c4f9bd5fccc3acf6ba3a44980c
cristina <yosola@gmail.com>
2018-08-31 15:59:45 -0700

    Fetching data correctly

--- blog/src/PostContainer.js
+++ blog/src/PostContainer.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
 import Postpreview from './Postpreview';
-import postsData from './data/postData';
+import * as contentful from 'contentful'
 
 
 class PostContainer extends Component{
 
   constructor(props) {
      super(props);
-     this.state = { posts: postsData };
+     this.state = {posts: []};
    }
 
+client = contentful.createClient({
+     space: 'on3t7p55b9vo',
+     accessToken: 'e5ec91831b489dc0e4d81a2755bf84e9e1ddc71c3147e0f2674ca21dfc454e27' })
+
+  componentDidMount() {
+      this.fetchPosts().then(this.setPosts);
+    }
+
+  fetchPosts = () => this.client.getEntries();
+
+  setPosts = response => {
+      this.setState({
+        posts: response.items
+
+      });
+
+
+    }
+
   render(){
-    const { posts } = this.state;
 
     return (
       <div className="container">
         <div className="row">
           <div className="col-lg-8 col-md-10 mx-auto">
-          {posts.map((post, index) =>
-            <Postpreview key={index}
-            post={post} />
+          { this.state.posts.map(({fields}, i) =>
+          <Postpreview key={i} post={fields} />
           )}
           <hr/>
           </div>
